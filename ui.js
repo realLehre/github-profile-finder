0c6e55ebed03f114cd66fa76f5094746eea97b66
David Egbuwalo <lereegbuwalo@gmail.com>
2022-03-22 15:04:15 +0100

    update

--- ui.js
+++ ui.js
@@ -34,7 +34,7 @@ class UI{
     showRepos(repos){
         let output = '';
 
-        repos.forEach(repo =>{
+        repos.forEach(function(repo) {
             output += `
                 <div class="card card-body mb-2">
                 <div class="row">
