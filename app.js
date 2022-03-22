ce81231fad6053e4acd1ca57e0d8f8509959d8df
David Egbuwalo <lereegbuwalo@gmail.com>
2022-03-22 12:49:33 +0100

    update

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// 01fe3484c2641765522126c94f868748b00f8141
-
 const searchUser = document.getElementById('searchUser');
 
 searchUser.addEventListener('keyup', (e) => {
--- github.js
+++ github.js
@@ -1,5 +1,10 @@
 class GitHub{
     constructor(){
-        
+        this.client_id = 'e6964645203ba172a0a2';
+        this.client_secret = '01fe3484c2641765522126c94f868748b00f8141';
+    }
+
+    async getUser(user){
+        const profileResponse = await fetch(`https://api.github.com/users/${user}`)
     }
 }
