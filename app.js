b8c81a0f94751b16c62faa2b36d7b44f639665cc
David Egbuwalo <lereegbuwalo@gmail.com>
2022-03-22 12:53:07 +0100

    update

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+// Init github
+const github = new GitHub();
+
 const searchUser = document.getElementById('searchUser');
 
 searchUser.addEventListener('keyup', (e) => {
     userName = e.target.value;
 
     if(userName){
-        
+        github.getUser(userName);
     }
 })
