664fa540cc0d9a29cc09f7dc3d6ede14ad80e4c7
David Egbuwalo <lereegbuwalo@gmail.com>
2022-03-22 14:51:43 +0100

    update

--- github.js
+++ github.js
@@ -2,6 +2,8 @@ class GitHub{
     constructor(){
         this.client_id = 'e6964645203ba172a0a2';
         this.client_secret = '01fe3484c2641765522126c94f868748b00f8141';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user){
@@ -9,7 +11,7 @@ class GitHub{
 
         const profile = await profileResponse.json();
 
-        const profileRepos = await fetch(`https://api.github.com/users/${user}/repos?client_id=${this.client_id}&client_secret=${this.client_secret}`)
+        const profileRepos = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}client_id=${this.client_id}&client_secret=${this.client_secret}`)
 
         const repos = await profileRepos.json();
 
