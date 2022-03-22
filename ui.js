7163e365cfe2026a0d60f37ff8e983ce4190ff0a
David Egbuwalo <lereegbuwalo@gmail.com>
2022-03-22 14:57:21 +0100

    update

--- ui.js
+++ ui.js
@@ -34,10 +34,20 @@ class UI{
     showRepos(repos){
         let output = '';
 
-        console.log(repos);
         repos.forEach(repo =>{
             output += `
-
+                <div class="card card-body mb-2">
+                <div class="row">
+                    <div class="col-md-6">
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                    </div>
+                    <div class="col-md-6">
+                    <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+                    <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+                    <span class="badge badge-success">Forks: ${repo.forms_count}</span>
+                    </div>
+                </div>
+                </div>
             `
         })
         
