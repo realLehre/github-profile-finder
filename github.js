ec3bf2c74d4e27f222b4c2d6a8342cce6289295c
David Egbuwalo <lereegbuwalo@gmail.com>
2022-03-22 12:54:32 +0100

    update

--- github.js
+++ github.js
@@ -7,8 +7,10 @@ class GitHub{
     async getUser(user){
         const profileResponse = await fetch(`https://api.github.com/users/${user}?'client_id=${this.client_id}&'client_secret=${this.client_secret}'`);
 
-        const profile = await profileResponse;
+        const profile = await profileResponse.json();
 
-        console.log(profile);
+        return{
+            profile
+        }
     }
 }
