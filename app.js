d98ab0b672d92b7066a75634c240ba10b9c586b5
David Egbuwalo <lereegbuwalo@gmail.com>
2022-03-22 14:06:08 +0100

    update

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ searchUser.addEventListener('keyup', (e) => {
             .then(data => {
                 if(data.profile.message === 'Not Found') {
                     // show error
+                    ui.showAlert('User not found', 'alert alert-danger');
                 } else {
                     ui.showUser(data.profile);
                 }
